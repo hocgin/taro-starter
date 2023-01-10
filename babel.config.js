79d3dfc8b015fd1d925196fb04cbf74548a42656
hocgin <hocgin@gmail.com>
2023-01-10 15:49:20 +0800

    update

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,21 @@
 // babel-preset-taro 更多选项和默认值：
 // https://github.com/NervJS/taro/blob/next/packages/babel-preset-taro/README.md
+
+
+function useLogger() {
+  let result = [];
+  let offLogger = process.env.USE_LOG !== 'true';
+  console.debug(`[${offLogger ? '禁用' : '启用'}]日志打印`);
+  if (offLogger) {
+    result.push([
+      'transform-remove-console',
+      {exclude: ['error', 'warn', 'info']},
+    ]);
+  }
+  return result;
+}
+
+
 module.exports = {
   presets: [
     ['taro', {
@@ -7,9 +23,12 @@ module.exports = {
       ts: true
     }]
   ],
-  plugins: [["import", {
-    "libraryName": "@antmjs/vantui",
-    "libraryDirectory": "es",
-    "style": true
-  }, "@antmjs/vantui"]]
+  plugins: [
+    ["import", {
+      "libraryName": "@antmjs/vantui",
+      "libraryDirectory": "es",
+      "style": true
+    }, "@antmjs/vantui"],
+    ...useLogger()
+  ]
 }
